e663cb9bace445659cc59d685969128b74b8495d
Seulgi Kim <dev@seulgi.kim>
2021-08-13 17:26:45 +0900

    Add @rollup/plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+import typescript from "@rollup/plugin-typescript";
+
 const dev = !!process.env.ROLLUP_WATCH;
 const dest = dev ? "build" : "dist";
 
 export default {
-    input: "src/main.js",
+    input: "src/main.ts",
     output: {
         file: `${dest}/bundle.js`,
         format: "iife",
     },
+    plugins: [typescript()],
 };
