25e8cd65f5feb48e5c078894baab34464a60f3d2
Seulgi Kim <dev@seulgi.kim>
2021-08-23 18:07:30 +0900

    Make dev command serve the files

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,43 @@
 import commonjs from "@rollup/plugin-commonjs";
 import copy from "rollup-plugin-copy";
 import resolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
 import svelte from "rollup-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 
 const dev = !!process.env.ROLLUP_WATCH;
 const dest = dev ? "build" : "dist";
+
+const plugins = [
+    copy({
+        targets: [{ src: "./public/**/*", dest }],
+    }),
+    svelte({
+        preprocess: sveltePreprocess({ sourceMap: dev }),
+        compilerOptions: {
+            dev,
+        },
+    }),
+    resolve({
+        browser: true,
+        dedupe: ["svelte"],
+    }),
+    commonjs(),
+    typescript({
+        sourceMap: dev,
+        inlineSources: dev,
+    }),
+];
+
+if (dev) {
+    plugins.push(
+        serve({
+            contentBase: "build",
+            port: 8888,
+        })
+    );
+}
 
 export default {
     input: "src/main.ts",
@@ -16,24 +47,5 @@ export default {
         file: `${dest}/bundle.js`,
         format: "iife",
     },
-    plugins: [
-        copy({
-            targets: [{ src: "./public/**/*", dest }],
-        }),
-        svelte({
-            preprocess: sveltePreprocess({ sourceMap: dev }),
-            compilerOptions: {
-                dev,
-            },
-        }),
-        resolve({
-            browser: true,
-            dedupe: ["svelte"],
-        }),
-        commonjs(),
-        typescript({
-            sourceMap: dev,
-            inlineSources: dev,
-        }),
-    ],
+    plugins,
 };
